bda26997ab2a4d9f3d52f7d085831a81adcb0c84
geistesk <github.jyf300hj@0x21.biz>
2016-03-26 14:50:09 +0100

    Improved error protection and added retries

--- README.md
+++ README.md
@@ -9,8 +9,23 @@ What am I talking about? The glorious [IPv6 Certification](https://ipv6.he.net/c
 Inspired by [HECT](https://github.com/tactmaster/HECT).
 
 
-## Test
-For using it you should have installed binaries of `sort`, `sed`, `traceroute`, `dig`, `whois` and `ping6` or a new `ping`-version with the `-6` flag.
+## Dependencies and Test
+For using it you should have installed binaries of…
+
+* `bash` (*4.3.042-4*)
+* `coreutils` (*8.25-1*)
+* `curl` (*7.47.1-2*)
+* `grep` (*2.24-1*)
+* `sed` (*4.2.2-3*)
+* `traceroute` (*2.0.21-1*)
+* `whois` (*5.2.11-1*)
+
+where the version is the Arch Linux-packageversion.
+Next to this software you also need `ping6` or a `ping`-version with the `-6`-flag and `dig`.
+Arch Linux shipps this software packaged as…
+
+* `iputils` (*20150815.1c59920-3*)
+* `bind-tools` (*9.10.3.P4-1*)
 
 First execute the script in your shell and check if it's working for you (*works for me*™).
 
@@ -25,20 +40,20 @@ chmod +x jashect.sh
 If you're running a GNU/Linux with systemd you could run it with the following units:
 
 ```bash
-# cp jashect.sh /usr/local/bin/jashect
-# chmod +x /usr/local/bin/jashect
-# cp -r systemd/* /usr/lib/systemd/system
+cp jashect.sh /usr/local/bin/jashect
+chmod +x /usr/local/bin/jashect
+cp -r systemd/* /usr/lib/systemd/system
 ```
 
 Now modify the content of `/usr/lib/systemd/system/jashect.service.d/local.conf`
 
 ```bash
-# systemctl daemon-reload
-# systemctl start jashect.timer
-# systemctl enable jashect.timer
+systemctl daemon-reload
+systemctl start jashect.timer
+systemctl enable jashect.timer
 ```
 
-There you go! You don't have to worry for the next 100 days. Maybe you want to disable the script after reaching 1.5k points because there are no brakes :^)
+There you go! You don't have to worry for the next 100 days. Maybe you want to disable the script after reaching 1.5k points because there are no brakes on the IPv6-Train :^)
 
 ### everything else
 You could write a daily cronjob but it may fail because sometimes the script takes 5secs and sometimes 10secs. So you won't wait exactly 24 hours and your submission was rejected.. So perhaps it's a good idea to write two cronjobs separated for 12h or something like that.
--- jashect.sh
+++ jashect.sh
@@ -2,15 +2,9 @@
 # Just Another Simplified Hurricane Electric Certification Tool
 # Usage: $0 username password
 
-SIXY_FEED=`curl -s http://sixy.ch/feed`
-HOST=`echo "$SIXY_FEED"| \
-  sed -n 's/\([ ]*\)<id>http:\/\/sixy.ch\/go\/\([[:alnum:].-]*\)<\/id>/\2/p' | \
-  sort -R | head -n 1`
-IPADDR=`echo "$SIXY_FEED" | \
-  sed -n 's/IPv6 address: \([[:xdigit:]:]*\)<\/summary>/\1/p' | \
-  sort -R | head -n 1`
+HOST=""
+IPADDR=""
 PING6=`type ping6 2> /dev/null && echo "ping6" || echo "ping -6"`
-
 PAGES_TITLE=(
   "traceroute"
   "aaaa"
@@ -18,20 +12,63 @@ PAGES_TITLE=(
   "ping"
   "whois")
 PAGES_ACTION=(
-  "traceroute -6 $HOST"
-  "dig AAAA $HOST"
-  "dig -x $IPADDR"
-  "$PING6 -c3 $IPADDR"
-  "whois $IPADDR")
-PAGES_FILES=()
+  "traceroute -6 <HOST>"
+  "dig AAAA <HOST>"
+  "dig -x <IPADDR>"
+  "$PING6 -c3 <IPADDR>"
+  "whois <IPADDR>")
 COOKIE_FILE=`mktemp`
+FAIL_RETRY=3
 
+function getTargets {
+  local sisy_feed=`curl -s http://sixy.ch/feed`
+  HOST=`echo "$sisy_feed" \
+  | sed -n 's/\([ ]*\)<id>http:\/\/sixy.ch\/go\/\([[:alnum:].-]*\)<\/id>/\2/p' \
+  | sort -R -u`
+  IPADDR=`echo "$sisy_feed" \
+  | sed -n 's/IPv6 address: \([[:xdigit:]:]*\)<\/summary>/\1/p' \
+  | sort -R -u`
+
+  local host_lines=`echo "$HOST" | wc -l`
+  local ipaddr_lines=`echo "$IPADDR" | wc -l`
+  if (( "$host_lines" <= "1" )) || (( "$ipaddr_lines" <= "1" )); then
+    echo "Could not fetch IPs and hostnames from sixy.ch"
+    rm $COOKIE_FILE
+    exit 1
+  fi
+}
+
+# $1: No of HOST/IPADDR
+function getHost()   { echo "$HOST"   | head -n $1 | tail -n 1; }
+function getIPAddr() { echo "$IPADDR" | head -n $1 | tail -n 1; }
+
+# $1: PAGES_*-ID, $2: No of HOST/IPADDR
 function invokeAction {
-  for (( i=0; i<${#PAGES_ACTION[@]}; i++ )); do
-    local target=`mktemp`
-    echo "`${PAGES_ACTION[i]}`" > $target
-    PAGES_FILES[$i]=$target
+  local target=`mktemp`
+  local ipaddr=`getIPAddr $2`
+  local host=`getHost $2`
+  local cmnd=`echo "${PAGES_ACTION[$1]}" \
+  | sed "s/<IPADDR>/$ipaddr/;s/<HOST>/$host/"`
+
+  echo "`$cmnd`" > $target
+  echo $target
+}
+
+# $1: PAGES_*-ID
+function postPage() {
+  for (( j=1; j<=$FAIL_RETRY; j++)); do
+    echo "Invoking ${PAGES_TITLE[$1]} for the $j. time.."
+
+    local tmp_file=`invokeAction $1 $j`
+    local he_resp=`hePost ${PAGES_TITLE[$1]} $tmp_file`
+    rm $tmp_file
+
+    echo "$he_resp"
+    if echo "$he_resp" | grep --quiet "succeeded"; then
+      break
+    fi
   done
+  echo "Finished trying ${PAGES_TITLE[$1]}"
 }
 
 # $1: Username, $2: Password
@@ -42,9 +79,11 @@ function heLogin {
                    "https://ipv6.he.net/certification/login.php"`
 
   if echo $resp | grep --quiet "errorMessageBox"; then
-    >&2 echo "Login for $1 failed!"
-    cleanUp
+    echo "Login for $1 failed!"
+    rm $COOKIE_FILE
     exit 1
+  else
+    echo "Login for $1 succeeded"
   fi
 }
 
@@ -57,29 +96,22 @@ function hePost {
               "https://ipv6.he.net/certification/daily.php?test=$1"`
 
   if echo $resp | grep --quiet "errorMessageBox"; then
-    local err_msg=`echo "$resp" | \
-      sed -n 's/<div class=\"errorMessageBox\">\(.*\)<\/div>/\1/p'`
-    >&2 echo "Posting $1 failed: $err_msg"
+    local err_msg=`echo "$resp" \
+    | sed -n 's/<div class=\"errorMessageBox\">\(.*\)<\/div>/\1/p'`
+    echo "Posting $1 failed: $err_msg"
   else
     echo "Posting $1 succeeded"
   fi
 }
 
-function cleanUp {
-  rm $COOKIE_FILE
-  for f in "${PAGES_FILES[@]}"; do
-    rm $f
-  done
-}
 
-
-[ "$#" != "2" ] && >&2 echo "Usage: $0 username password" && exit 1
+[ "$#" != "2" ] && echo "Usage: $0 username password" && exit 1
 
 heLogin "$1" "$2"
-invokeAction
+getTargets
 
 for (( i=0; i<${#PAGES_TITLE[@]}; i++ )); do
-  hePost ${PAGES_TITLE[i]} ${PAGES_FILES[i]}
+  postPage $i #&
 done
 
-cleanUp
+rm $COOKIE_FILE
